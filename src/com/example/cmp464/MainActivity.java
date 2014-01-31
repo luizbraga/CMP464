7be495cb08d84f7b1f38001d759137a9326feae7
Luiz Braga <luizbragaa@gmail.com>
2014-01-31 02:20:23 -0500

    Added an activity in the button

--- src/com/example/cmp464/MainActivity.java
+++ src/com/example/cmp464/MainActivity.java
@@ -3,13 +3,32 @@ package com.example.cmp464;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	
+	EditText text;
+	TextView textV;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final Button button = (Button) findViewById(R.id.button1);
+		text = (EditText) findViewById(R.id.text);
+		textV = (TextView) findViewById(R.id.textView);
+		
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				textV.setText(MainActivity.this.text.getText().toString());
+			}
+		});
 	}
 
 	@Override
@@ -18,5 +37,9 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	public void change(View view){
+		
+	}
+	
 }
